36b019f18edcd9ad6b57a366fe6ffa37248d7d98
Dmitrii Abramov <dmitrii@abramov.io>
2016-05-14 15:13:27 -0700

    gitignore

--- packages/777/build/invariant.js
+++
@@ -1,8 +0,0 @@
-'use strict';Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
-
-invariant;function invariant(
-assertion) 
-
-{var message = arguments.length <= 1 || arguments[1] === undefined ? 'Invariant violation' : arguments[1];
-  if (!assertion) {
-    throw new Error(message);}}
--- packages/777/build/utils.js
+++
@@ -1,4 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.
-
-last = last;function last(array) {
-  return array[array.length - 1];}
